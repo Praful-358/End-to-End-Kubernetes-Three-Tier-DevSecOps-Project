32ee441e941f70429423ab9d217185357b7a707d
Praful-358 <114816486+Praful-358@users.noreply.github.com>
2026-08-15 17:34:01 +0530

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "s3-devsecops-pro-bucket"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "eks-argo-dynamodb"
     encrypt        = true
   }
   required_version = ">=0.13.0"
